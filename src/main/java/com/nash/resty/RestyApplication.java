9d08ef4a95e8e2b3c83aad50b1754a5c96630d59
Your Name <a.bat@vikes.csuohio.edu>
2022-06-10 15:29:18 -0400

    Initial commit

---
+++ src/main/java/com/nash/resty/RestyApplication.java
@@ -0,0 +1,13 @@
+package com.nash.resty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nash/resty/RestyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nash.resty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
